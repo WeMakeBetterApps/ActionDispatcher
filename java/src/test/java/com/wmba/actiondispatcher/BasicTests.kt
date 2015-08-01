1aace967c7c97f549c47b3b17c4c87578ab617c1
Scott Pierce <spierce7@gmail.com>
2015-08-01 17:15:24 -0400

    Some more basic tests.

--- java/src/test/java/com/wmba/actiondispatcher/BasicTests.kt
+++ java/src/test/java/com/wmba/actiondispatcher/BasicTests.kt
@@ -4,6 +4,8 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 public class BasicTests {
@@ -27,6 +29,61 @@ public class BasicTests {
     val activeKeys = dispatcher.getActiveKeys();
     assertEquals(1, activeKeys.size())
     assertTrue(activeKeys.contains(KeySelector.DEFAULT_KEY))
+  }
+
+  @Test fun runAsyncTest() {
+    var count = 0;
+
+    var action1TheadId: Long? = null
+    var action2TheadId: Long? = null
+    var action1TheadName: String? = null
+    var action2TheadName: String? = null
+
+    dispatcher.toSingle(object: Action<Boolean>() {
+      override fun execute(): Boolean {
+        Thread.sleep(10)
+        count++;
+        assertEquals(2, count)
+
+        action1TheadId = Thread.currentThread().getId()
+        action1TheadName = Thread.currentThread().getName()
+
+        return true
+      }
+    }).subscribe()
+
+    dispatcher.toSingle(object: Action<Boolean>(key = "key2") {
+      override fun execute(): Boolean {
+        count++;
+        assertEquals(1, count)
+
+        action2TheadId = Thread.currentThread().getId()
+        action2TheadName = Thread.currentThread().getName()
+
+        return true
+      }
+    }).subscribe()
+
+    block()
+
+    assertEquals(2, count)
+
+    assertNotNull(action1TheadId)
+    assertNotNull(action2TheadId)
+    assertNotNull(action1TheadName)
+    assertNotNull(action2TheadName)
+    assertNotEquals(action1TheadId, action2TheadId)
+    assertNotEquals(action1TheadName, action2TheadName)
+  }
+
+  @Test fun returnValueTest() {
+    val returnValue = dispatcher.toSingle(object: Action<String>() {
+      override fun execute(): String {
+        return "value"
+      }
+    }).toObservable().toBlocking().first()
+
+    assertEquals("value", returnValue)
   }
 
   fun block() {
